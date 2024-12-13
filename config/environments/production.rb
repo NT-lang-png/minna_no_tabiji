49bddd06edd692a9df62ea3639be2781bb05661b
EC2 Default User <ec2-user@ip-172-31-5-60.ap-northeast-1.compute.internal>
2024-12-13 04:57:46 +0000

    [finish]アプリ名変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "shiori_production"
+  # config.active_job.queue_name_prefix = "minna_no_tabiji_production"
 
   config.action_mailer.perform_caching = false
 
