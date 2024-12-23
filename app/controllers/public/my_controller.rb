ec24a7136a6c5492f306db10b5b90b858e2b7fad
EC2 Default User <ec2-user@ip-172-31-5-60.ap-northeast-1.compute.internal>
2024-12-23 18:02:54 +0000

    style:レイアウト調整

--- app/controllers/public/my_controller.rb
+++ app/controllers/public/my_controller.rb
@@ -5,16 +5,14 @@ class Public::MyController < ApplicationController
   
   def my_index
     #カレントユーザーの新着投稿一覧、行き先登録が無くてもしおりを全表示する
-    case params[:status]
+    @status = params[:status]
+    case @status
     when 'published'
       @itineraries = @user.itineraries.where(status: :published).order(id: :desc).page(params[:page]).per(6)
-      @status = params[:status]
     when 'unpublished'
       @itineraries = @user.itineraries.where(status: :unpublished).order(id: :desc).page(params[:page]).per(6)
-      @status = params[:status]
     else
       @itineraries = @user.itineraries.where(status: :draft).order(id: :desc).page(params[:page]).per(6)
-      @status = params[:status]
     end
   end
 
--- app/models/destination.rb
+++ app/models/destination.rb
@@ -4,14 +4,15 @@ class Destination < ApplicationRecord
   validates :start_time ,presence:true
   validates :name,presence:true , length: { minimum: 1, maximum: 30 }
 
+  #しおりの日程の範囲内で行き先の日程を選ぶメソッド
+  validate :day_number_within_itinerary_range
+
+
   #MAP
   validates :address, presence: true
-
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
 
-  #しおりの日程の範囲内で行き先の日程を選ぶメソッド
-  validate :day_number_within_itinerary_range
 
   attribute :day_number, :integer
   belongs_to :itinerary
@@ -20,6 +21,13 @@ class Destination < ApplicationRecord
   #日程順、開始時間順に並び替えメソッド
   scope :ordered, -> { order(:day_number, :start_time) }
 
+  private
+
+  def day_number_within_itinerary_range
+    if day_number < 1 || day_number > itinerary.day_number
+      errors.add(:day_number, "はしおりの日程内で指定してください")
+    end
+  end
 
 
 end
