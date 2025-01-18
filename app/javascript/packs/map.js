29a81412698ed61790e00957caf574fb08b9cc49
NT-lang-png <183378925+NT-lang-png@users.noreply.github.com>
2025-01-18 13:11:14 +0000

    fix:マップ中心表示機能修正　経度

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -35,7 +35,7 @@ async function initMap() {
     //jsonファイル内のデータを定義
     const data = await response.json();
     const  { data: { items } } = data;
-    let earliest = { latitude: tokyoLatitude, longitude: tokyoLatitude }
+    let earliest = { latitude: tokyoLatitude, longitude: tokyoLongitude }
 
 
     if (!Array.isArray(items)) throw new Error("Items is not an array");
@@ -46,37 +46,15 @@ async function initMap() {
 
     // 地図の中心を設定
 
-    // // destinationの中から、一番行き先順の早いものを抽出し、定義する
-
-
-
-    // if (addresses.every(address => address)) {
-    //   //  addressカラムがすべて入っている場合
-    //   console.log("すべてのdestinationsにaddressが存在します。");
-    //   const earliest = 
-    
-    // } else if (addresses.some(address => address) && addresses.some(address => !address)) {
-    //   //  addressカラムがあるものとないものが混在している場合
-    //   const destinationsWithAddress = itinerary.destinations.filter(dest => dest.address);
-    //   console.log("addressがあるものとないものが混在しています。");
-    
-    // } else if (addresses.every(address => !address)) {
-    //   //  すべてのaddressカラムがない場合
-    //   console.log("すべてのdestinationsにaddressがありません。");
-    // }
-
-
-
-
-
 
     const trueAddressItems = items.filter(item => (item.address != ""))
+    console.log(trueAddressItems)
     if (trueAddressItems.length != 0) {
       earliest = trueAddressItems[0]
     }
 
-    console.log(items.filter(item => (item.address != "")))
     const center = { lat: earliest.latitude, lng: earliest.longitude }
+    console.log(center)
     //? { lat: earliest.latitude, lng: earliest.longitude }
     //: { lat: tokyoLatitude, lng: tokyoLongitude };  // 行き先がなければ東京駅のデフォルト座標
     //console.log("Map center:", center);      // デバッグ用
